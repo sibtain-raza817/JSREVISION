c71b924128243bbbde25bec64d68d6298dc80063
Sibtain Raza <khanbhai14063@gmail.com>
2026-06-14 07:47:05 +0530

    js revision start

---
+++ basic/app.js
@@ -0,0 +1,52 @@
+// How to declare a variables
+
+//Old way to dec
+// var a = 10;   
+// console.log(a);
+
+// //2.
+// let b = 10;
+// console.log(b);
+
+// //3.
+// const c = 20;
+// console.log(c);
+
+//Diff b/w all these variables
+
+// var :- var can be redeclare and reinitilization also 
+//ex,  
+// var a = 10;
+// console.log(a);
+
+// var a = 20;  // redeclared
+// console.log(a);
+
+// // re initilization
+// a = 30;   // both will run correctly
+// console.log(a);
+
+// let :- it can be only reinitilization only, it can not be redeclared 
+
+//ex,
+// let b = 10;
+// b = 20; // it will run correctly 
+// console.log(b);
+
+// // it not allowing to redeclare
+// let b = 60;
+// console.log(b);  // This will through error 
+
+// const  :-  const not allowing both redeclaration and reiniti....
+//ex,
+/*
+const c = 30;
+c = 40; // not allowing    Assignment to constant variable.
+const c = 50 // not allowing
+*/
+
+
+
+
+
+
