f7f25bd77e6cb9c10d81de4b3e618179547b9fc2
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-03 10:00:44 +0530

    logical operator

--- conditionals/app.js
+++ conditionals/app.js
@@ -19,6 +19,7 @@ else{
 
 // 2 example
 
+/*
 let age = 19
 
 if(age > 18){
@@ -27,4 +28,34 @@ if(age > 18){
     console.log("You just turned 18, welcome");
 }else{
     console.log(`you are ${age} years old, get outta here`);
+}
+
+*/
+
+
+//Comparison Operator
+
+/*
+&& check if its left and right side are true
+
+true && true == true
+true && false == false
+false && true == false 
+false && false == false
+*/
+
+//
+let age = 19;
+let hasId = false;
+
+// if(age >= 18 && hasId === true){
+//     console.log("You can entered the club")
+// }else{
+//     console.log("you can not entered the club");
+// }
+
+//or logical op
+
+if(age >= 17 || hasId === true){
+    console.log("you may entered the club")
 }
