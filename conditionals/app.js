fd05d8674801a0fcb3dd7af39980e260f025ce11
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-03 09:13:46 +0530

    check membership

---
+++ conditionals/app.js
@@ -0,0 +1,16 @@
+// 1 example 
+
+let hasMembership = false
+let signedIn = true
+
+if(hasMembership === true){
+    console.log("show video the user");
+}
+else if(signedIn === true){
+    console.log("tell usert to upgrade their account");
+    
+}
+else{
+    console.log("tell user to login in");
+    
+}
