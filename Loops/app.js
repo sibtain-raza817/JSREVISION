d30dcfa53d3c32bc3cb3f8cfe1409be9c1ebd07a
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-03 11:08:33 +0530

    Loops practice

---
+++ Loops/app.js
@@ -0,0 +1,46 @@
+//Loops in js
+
+//traditional methode
+// print 1 to 20
+
+/*
+//massive code
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+console.log(11);
+console.log(12);
+console.log(13);
+console.log(14);
+console.log(15);
+console.log(16);
+console.log(17);
+console.log(18);
+console.log(19);
+console.log(20);
+
+*/
+
+// let counter = 1;
+
+// while(counter <= 20){
+//     console.log(counter);
+//     counter = counter+1;  
+// }
+
+// console.log("while loop finished");
+
+
+//for loop
+
+for(let i=0; i<5; i++){
+    console.log(i);
+    
+}
