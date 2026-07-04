d914fe26bcf101aafe6f46d0a7503aa1f1b619b6
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-04 10:39:52 +0530

    functions practice

---
+++ functions/app.js
@@ -0,0 +1,11 @@
+//functions
+
+function greeting(name) {  //function declararion 
+    console.log(`Hello ${name}`);  
+}
+function greeting2(name, time) {  //function declararion 
+    console.log(`Good ${time} ${name}`);  
+}
+//call the function
+greeting("Raza")
+greeting2("Raza", "Morning")
