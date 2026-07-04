a9aeb589c038ddc4dbba4fc4a76fa743aaf577fa
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-04 11:08:48 +0530

    Arrays in js

---
+++ arrays/app.js
@@ -0,0 +1,9 @@
+
+// TRADITIONAL WAY TO STORE THE ITEMS 
+
+let items1 = "Apples";
+let items2 = "Banana";
+let items3 = "Kiwi";
+let items4 = "Eggs";
+
+console.log(items1, items2, items3, items4);
