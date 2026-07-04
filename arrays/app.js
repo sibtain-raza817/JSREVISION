4131d30748f7369a10f0914a6c0588198e7b073c
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-04 12:14:39 +0530

    Array Done

--- arrays/app.js
+++ arrays/app.js
@@ -11,7 +11,7 @@ console.log(items1, items2, items3, items4);
 */
 
 // how arrays short this problem :- array means store the items in a single varibale with same  data types of value. 
-
+/*
 let items = ["Apples", "Banana", "Kiwi", "Eggs"];
 
 // first element of array 
@@ -39,3 +39,20 @@ let array = [18,16,15,20,21]
 //same with short
 let Adult = array.filter((ele) => ele >= 18)
 console.log(filteredArray)
+
+*/
+
+//Loop in array
+
+let array = [18,16,15,20,21];
+let adult = [];
+
+for(let i=0; i<array.length; i++){
+    //console.log(array[i]);
+    if(array[i] >= 18){
+        //var result = adult.push(array[i]);
+        adult.push(array[i]); 
+    }
+}
+
+console.log(adult);
