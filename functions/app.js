ad5b6128892f1a7235711c315c97fc4ddf9f61bc
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-04 11:01:19 +0530

    functions practice

--- functions/app.js
+++ functions/app.js
@@ -1,5 +1,5 @@
 //functions
-
+/*
 function greeting(name) {  //function declararion 
     console.log(`Hello ${name}`);  
 }
@@ -9,3 +9,18 @@ function greeting2(name, time) {  //function declararion
 //call the function
 greeting("Raza")
 greeting2("Raza", "Morning")
+
+*/
+function sum(num1, num2){
+    return num1 + num2;
+}
+console.log(sum(10, 20));
+
+function convertUsdToAud(dollars){
+    let AUD = dollars * 1.5;
+    return AUD;
+}
+
+console.log(convertUsdToAud(1000));
+
+
