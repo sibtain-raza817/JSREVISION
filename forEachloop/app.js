461d8e6b826980b48db544c91b71922198bfae99
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-05 23:40:45 +0530

    forEach loop in js

---
+++ forEachloop/app.js
@@ -0,0 +1,26 @@
+let coding = ["python", "cpp", "java", "ruby", "js"];
+
+
+let values = coding.forEach((items)=>{
+    console.log(items);
+    return items
+})
+
+console.log(values);
+/*
+forEach does not return any value
+python
+cpp
+java
+ruby
+js
+undefined
+*/
+
+//but filter method return new array means return somthings
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+let newNums = myNums.filter((num) => num > 5);
+
+console.log(newNums);
