0c5be90fcf5ee8198d471686078cda91472a062c
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-07 23:13:16 +0530

    return value in filter methode

--- forEachloop/app.js
+++ forEachloop/app.js
@@ -1,12 +1,12 @@
-let coding = ["python", "cpp", "java", "ruby", "js"];
+// let coding = ["python", "cpp", "java", "ruby", "js"];
 
 
-let values = coding.forEach((items)=>{
-    console.log(items);
-    return items
-})
+// let values = coding.forEach((items)=>{
+//     console.log(items);
+//     return items
+// })
 
-console.log(values);
+// console.log(values);
 /*
 forEach does not return any value
 python
@@ -21,6 +21,11 @@ undefined
 
 const myNums = [1,2,3,4,5,6,7,8,9,10];
 
-let newNums = myNums.filter((num) => num > 5);
+// let newNums = myNums.filter((num) => num > 5);
 
-console.log(newNums);
+// return karna padega agr is trah likhoge tab
+// agr curly bracket laga diya means : you have to return 
+let nums = myNums.filter((num)=>{
+    return num > 6;
+})
+console.log(nums);
