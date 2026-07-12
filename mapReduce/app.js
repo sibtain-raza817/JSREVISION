c288724071104fe0defeba67d754f9d03a1c5d3a
Sibtain Raza <khanbhai14063@gmail.com>
2026-07-12 10:44:21 +0530

    reduce method

--- mapReduce/app.js
+++ mapReduce/app.js
@@ -1,10 +1,20 @@
 // chaining in map, filter
 
-let myNums = [1,2,3,4,5,6,7,8,9,10];
+// let myNums = [1,2,3,4,5,6,7,8,9,10];
 
-let result = myNums
-                .map((num) => num * 10)
-                .map((num) => num + 1)
-                .filter((num) => {return num >= 20});
+// let result = myNums
+//                 .map((num) => num * 10)
+//                 .map((num) => num + 1)
+//                 .filter((num) => {return num >= 20});
 
-console.log(result);
+// console.log(result);
+
+// Reduce method
+
+const array = [1,2,3,4,5,6,7,8];
+
+const sumOfTotalNum = array.reduce((acc, currVal) =>{
+    console.log(`AccVal: ${acc}, CurrVal: ${currVal}`);
+    
+    return acc + currVal
+}, 0)  // initilization of first value which will go in acc 
