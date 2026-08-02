d17e4f2663ecfe057bfc33cf16655ad0104657fd
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-02 21:55:37 +0530

    ES6 module in js (import, export)

--- import_export/script.js
+++ import_export/script.js
@@ -1,5 +1,12 @@
 import { multiply } from "./utility.js";
+import { addition } from "./utility.js";
 
 const result = multiply(2,5,8);
 
 console.log(result);
+
+const add = addition(5, 10);
+
+console.log(add);
+
+
--- import_export/utility.js
+++ import_export/utility.js
@@ -1,3 +1,11 @@
 export function multiply(a, b, c){
     return a+b+c;
 }
+
+function addition(a, b){
+    return a+b;
+}
+
+export{
+    addition
+}
